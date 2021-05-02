8f31fe5e6c96ead5724e47d6a3e5b6392ee4a582
Mathias Sammer <mathias.sammer.946@gmail.com>
2021-05-02 20:06:56 -0300

    Meu primeiro commit da informação em json.

---
+++ app.js
@@ -0,0 +1,16 @@
+//Informações pessoais sobre mim
+
+let pessoa = {
+  nome: 'Mathias Sammer',
+  idade: 23,
+  ensino_medio: true,
+  ensino_superior: true,
+  profissão: 'Programador Júnior',
+  Estudante: 'Digital House Brasil',
+  Curso: 'Web full stack',
+  assuntos_abordados: ['javascript', 'html5', 'css3', 'json', 'nodejs', 'express', 'mysql', 'react', 'npm', 'git', 'github'],
+  objetivo: 'Me tornar um excelente programador sênior.'
+}
+
+const info = JSON.stringify(pessoa);
+console.log(info);
